ed988285ef5f8991897f2808fb801aa9cf04efb5
SquidDev <bonzoweb@hotmail.co.uk>
2015-01-19 10:35:32 +0000

    Refactor some API wrapping into a separate file

---
+++ src/main/java/squidev/ccstudio/core/apis/CCAPIWrapper.java
@@ -0,0 +1,55 @@
+package squidev.ccstudio.core.apis;
+
+import org.luaj.vm2.LuaError;
+import org.luaj.vm2.LuaFunction;
+import org.luaj.vm2.LuaTable;
+import org.luaj.vm2.Varargs;
+import org.luaj.vm2.lib.VarArgFunction;
+
+/**
+ * A wrapper for using one `invoke` method for
+ * multiple lua methods
+ */
+public abstract class CCAPIWrapper extends CCAPI {
+	protected String[][] methodNames = null;
+	protected String[] names = null;
+
+	/**
+	 * Returns the variables this API should be stored in
+	 *
+	 * @return List of variable names
+	 */
+	public String[] getNames() {
+		return names;
+	}
+
+	public abstract Varargs invoke(Varargs args, int index);
+
+	/**
+	 * Convert this to a Lua table
+	 *
+	 * @return The API object
+	 */
+	protected LuaTable createTable() {
+		LuaTable table = new LuaTable();
+		try {
+			for (int i = 0, n = methodNames.length; i < n; i++) {
+				final int index = i;
+				LuaFunction f = new VarArgFunction() {
+					public Varargs invoke(Varargs args) {
+						return CCAPIWrapper.this.invoke(args, index);
+					}
+				};
+				f.setfenv(env);
+
+				// Allow multiple
+				for (String name : methodNames[i]) {
+					table.set(name, f);
+				}
+			}
+		} catch (Exception e) {
+			throw new LuaError("Bind failed: " + e);
+		}
+		return table;
+	}
+}
--- src/main/java/squidev/ccstudio/core/apis/wrapper/APIWrapper.java
+++ src/main/java/squidev/ccstudio/core/apis/wrapper/APIWrapper.java
@@ -1,61 +1,14 @@
 package squidev.ccstudio.core.apis.wrapper;
 
-import org.luaj.vm2.LuaError;
-import org.luaj.vm2.LuaFunction;
-import org.luaj.vm2.LuaTable;
-import org.luaj.vm2.Varargs;
-import org.luaj.vm2.lib.VarArgFunction;
-import squidev.ccstudio.core.apis.CCAPI;
+import squidev.ccstudio.core.apis.CCAPIWrapper;
 
 /**
  * Basic wrapper for APIs built with {@see APIBuilder}
  */
-public abstract class APIWrapper<T> extends CCAPI {
+public abstract class APIWrapper<T> extends CCAPIWrapper {
 	public final T instance;
-
-	protected String[][] methodNames = null;
-	protected String[] names = null;
-
-	protected LuaTable table;
 
 	public APIWrapper(T inst) {
 		instance = inst;
-	}
-
-	/**
-	 * Returns the variables this API should be stored in
-	 * @return List of variable names
-	 */
-	public String[] getNames() {
-		return names;
-	}
-
-	public abstract Varargs invoke(Varargs args, int index);
-
-	/**
-	 * Convert this to a Lua table
-	 * @return The API object
-	 */
-	protected LuaTable createTable() {
-		LuaTable table = new LuaTable();
-		try {
-			for (int i = 0, n = methodNames.length; i < n; i++) {
-				final int index = i;
-				LuaFunction f = new VarArgFunction() {
-					public Varargs invoke(Varargs args) {
-						return APIWrapper.this.invoke(args, index);
-					}
-				};
-				f.setfenv(env);
-
-				// Allow multiple
-				for (String name : methodNames[i]) {
-					table.set(name, f);
-				}
-			}
-		} catch ( Exception e ) {
-			throw new LuaError("Bind failed: " + e);
-		}
-		return table;
 	}
 }
