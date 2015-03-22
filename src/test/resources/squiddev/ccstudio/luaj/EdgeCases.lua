782d666084a655a9acf4ef94989d77474419eccd
SquidDev <bonzoweb@hotmail.co.uk>
2015-03-22 17:25:49 +0000

    Update EdgeCases with another issue

--- src/test/resources/squiddev/ccstudio/luaj/EdgeCases.lua
+++ src/test/resources/squiddev/ccstudio/luaj/EdgeCases.lua
@@ -1,6 +1,11 @@
 -- Edge case when a parameter assignment occurs after a non-returning branch statement in a VarArgsFunction subclass
 
-local function arg(a, b, ...)
+local function func(a, b, ...)
 	if a then error() end
+	b = b + 1
+end
+
+local function func(a, b, ...)
+	if a then b = 0 end
 	b = b + 1
 end
